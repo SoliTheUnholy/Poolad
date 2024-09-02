b6d19ff8f7093d8c1131b93abbdd0324dae3cd00
SoliTheUnholy <soheilzss82@gmail.com>
2024-09-02 18:37:00 +0330

    bug fix

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,6 @@
 "use server";
 import { connectDB } from "@/lib/mongodb";
-import User from "@/models/user";
+import user from "@/models/user";
 import bcrypt from "bcryptjs";
 import { signIn } from "next-auth/react";
 
@@ -8,18 +8,18 @@ export const register = async (values: any) => {
   const { password, number } = values;
   try {
     await connectDB();
-    const Euser = await User.findOne({ number });
+    const Euser = await user.findOne({ number });
     if (Euser) {
       return {
         error: "Number is used",
       };
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
+    const User = new user({
       number,
       password: hashedPassword,
     });
-    const savedUser = await user
+    const savedUser = await User
       .save()
       .then(signIn("credentials", { number, password: hashedPassword }));
   } catch (e: any) {
