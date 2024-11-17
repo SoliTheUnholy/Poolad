c2067091ce0a964e1417e577c948a3934095168b
SoliTheUnholy <soheilzss82@gmail.com>
2024-11-17 13:41:45 +0330

    popover

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,6 +3,7 @@ const { MONGODB_URI } = process.env;
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(MONGODB_URI as string);
+
     if (connection.readyState === 1) {
       return Promise.resolve(true);
     }
